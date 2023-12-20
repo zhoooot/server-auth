b6496c976c8045f96038e055773bc02f10dbf142
ntl22 <nxtung21@outlook.com>
2023-12-20 17:15:45 +0700

    add github action files

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:alpine
+WORKDIR /app
+
+COPY package*.json .
+RUN npm ci
+
+COPY . .
+
+EXPOSE 4006
+CMD ["npm", "start"]
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import configs from '../configs/configs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [process.env.RABBITMQ_URL],
-      queue: configs.rmq.auth.queueUpdate,
-      queueOptions: {
-        durable: false,
-      },
-    },
-  });
-
   await app.listen(4006);
 }
 bootstrap();
